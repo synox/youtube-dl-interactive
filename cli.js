8c0c11a1cb1721fb6f7e9a38f219052552fb26fa
synox <synox@user.github.org>
2019-05-27 00:55:45 +0800

    cleanup

--- cli.js
+++ cli.js
@@ -2,11 +2,10 @@
 'use strict'
 const meow = require('meow')
 const logSymbols = require('log-symbols')
-const shell = require('shelljs');
-const parseColumns = require('parse-columns');
-const ora = require('ora');
-const _ = require('lodash');
-const { askExtension, askIncludeSubs, askResolution } = require('./questions');
+const shell = require('shelljs')
+const parseColumns = require('parse-columns')
+const ora = require('ora')
+const {askExtension, askIncludeSubs, askResolution} = require('./questions')
 
 const cli = meow(
 	`Usage: youtube-dl-interactive URL
@@ -15,24 +14,28 @@ const cli = meow(
 
 async function init(args) {
 	if (!shell.which('youtube-dl')) {
-		shell.echo('Sorry, this script requires youtube-dl.');
-		shell.echo('See https://github.com/ytdl-org/youtube-dl.');
-		shell.exit(1);
+		shell.echo('Sorry, this script requires youtube-dl.')
+		shell.echo('See https://github.com/ytdl-org/youtube-dl.')
+		shell.exit(1)
 	}
 
 	if (args.length !== 1) {
 		cli.showHelp(1)
 	}
+
 	const url = args[0]
 
-	let formatSelection = await selectFormat(url);
+	const formatSelection = await selectFormat(url)
 
 	let options = '-f ' + formatSelection.format
 	if (await askIncludeSubs()) {
 		options += ' --all-subs --embed-subs'
 	}
 
-	console.log(logSymbols.success, 'OK, we will download format with number ' + formatSelection.format)
+	console.log(
+		logSymbols.success,
+		'OK, we will download format with number ' + formatSelection.format
+	)
 	shell.exec(`youtube-dl ${options} "${url}"`)
 }
 
@@ -42,32 +45,34 @@ init(cli.input).catch(error => {
 })
 
 async function selectFormat(url) {
-	const formats = await fetchFormatOptions(url);
-	let remainingFormats = formats;
+	const formats = await fetchFormatOptions(url)
+	let remainingFormats = formats
 
-	// by default, we ignore video only files
-	remainingFormats = remainingFormats.filter(f => f.note.indexOf('video only') === -1);
+	// By default, we ignore video only files
+	remainingFormats = remainingFormats.filter(
+		f => f.note.indexOf('video only') === -1
+	)
 
-	const resolution = await askResolution(remainingFormats);
-	remainingFormats = remainingFormats.filter(f => f.resolution == resolution);
+	const resolution = await askResolution(remainingFormats)
+	remainingFormats = remainingFormats.filter(f => f.resolution === resolution)
 
-	const extension = await askExtension(remainingFormats);
-	remainingFormats = remainingFormats.filter(f => f.extension == extension);
+	const extension = await askExtension(remainingFormats)
+	remainingFormats = remainingFormats.filter(f => f.extension === extension)
 
-	// hopefully only one format remains here.
-	return remainingFormats[0];
+	// Hopefully only one format remains here.
+	return remainingFormats[0]
 }
 
 async function fetchFormatOptions(url) {
 	const spinner = ora('Loading formats').start()
-	const qualitiesOutput = shell.exec(`youtube-dl -F "${url}"`, { silent: true }).stdout;
+	const qualitiesOutput = shell.exec(`youtube-dl -F "${url}"`, {silent: true})
+		.stdout
 	spinner.stop()
 
 	const withoutLogs = qualitiesOutput
-		.split("\n")
-		.filter(line => line.indexOf("[") !== 0)
-		.join("\n");
+		.split('\n')
+		.filter(line => line.indexOf('[') !== 0)
+		.join('\n')
 
-	return parseColumns(withoutLogs, {});
-
+	return parseColumns(withoutLogs, {})
 }
--- questions.js
+++ questions.js
@@ -1,38 +1,38 @@
-const inquirer = require('inquirer');
-const _ = require('lodash');
+const inquirer = require('inquirer')
+const _ = require('lodash')
 
-exports.askResolution = async function (formats) {
-    const answers = await inquirer.prompt([
-        {
-            type: 'list',
-            name: 'q',
-            message: 'Select resolutio:n',
-            choices: _.uniq(formats.map(f => f.resolution))
-        },
-    ]);
-    return answers.q;
+exports.askResolution = async function(formats) {
+	const answers = await inquirer.prompt([
+		{
+			type: 'list',
+			name: 'q',
+			message: 'Select resolutio:n',
+			choices: _.uniq(formats.map(f => f.resolution))
+		}
+	])
+	return answers.q
 }
 
-exports.askExtension = async function (formats) {
-    const answers = await inquirer.prompt([
-        {
-            type: 'list',
-            name: 'q',
-            message: 'Select extension:',
-            choices: formats.map(f => f.extension + " | " + f.note)
-        },
-    ]);
-    return answers.q.split(' |')[0];
+exports.askExtension = async function(formats) {
+	const answers = await inquirer.prompt([
+		{
+			type: 'list',
+			name: 'q',
+			message: 'Select extension:',
+			choices: formats.map(f => f.extension + ' | ' + f.note)
+		}
+	])
+	return answers.q.split(' |')[0]
 }
 
-exports.askIncludeSubs = async function () {
-    const answers = await inquirer.prompt([
-        {
-            type: 'confirm',
-            name: 'q',
-            message: 'Include subtitles?',
-            default: true
-        },
-    ]);
-    return answers.q
+exports.askIncludeSubs = async function() {
+	const answers = await inquirer.prompt([
+		{
+			type: 'confirm',
+			name: 'q',
+			message: 'Include subtitles?',
+			default: true
+		}
+	])
+	return answers.q
 }
