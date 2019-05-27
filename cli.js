dd9a45503774daaf5a8ef7a1f2b24ae2fdf62e31
synox <synox@user.github.org>
2019-05-28 07:49:53 +0800

    code cleanup

--- cli.js
+++ cli.js
@@ -5,24 +5,21 @@ const logSymbols = require('log-symbols')
 const shell = require('shelljs')
 const parseColumns = require('parse-columns')
 const ora = require('ora')
-const { filterByExtension, askIncludeSubs, filterByResolution, selectOne } = require('./questions')
-const updateNotifier = require('update-notifier');
-const pkg = require('./package.json');
-
+const updateNotifier = require('update-notifier')
+const questions = require('./questions')
+const pkg = require('./package.json')
 
 const cli = meow(`Usage: youtube-dl-interactive URL
-	 ` )
-
+	 `)
 
 async function init(args) {
-
 	if (!shell.which('youtube-dl')) {
 		shell.echo('Sorry, this script requires youtube-dl.')
 		shell.echo('See https://github.com/ytdl-org/youtube-dl.')
 		shell.exit(1)
 	}
 
-	updateNotifier({ pkg }).notify();
+	updateNotifier({pkg}).notify()
 
 	if (args.length !== 1) {
 		cli.showHelp(1)
@@ -31,10 +28,9 @@ async function init(args) {
 	const url = args[0]
 
 	const formatSelection = await selectFormat(url)
-
 	if (formatSelection) {
 		let options = '-f ' + formatSelection.format
-		if (await askIncludeSubs()) {
+		if (await questions.askIncludeSubs()) {
 			options += ' --all-subs --embed-subs'
 		}
 
@@ -44,7 +40,6 @@ async function init(args) {
 		)
 		shell.exec(`youtube-dl ${options} "${url}"`)
 	}
-
 }
 
 init(cli.input).catch(error => {
@@ -57,45 +52,54 @@ async function selectFormat(url) {
 	if (!formats) {
 		return null
 	}
+
 	let remainingFormats = formats
 
 	// By default, we ignore 'video only' files
 	remainingFormats = remainingFormats.filter(
 		f => f.note.indexOf('video only') === -1
 	)
-	
-	remainingFormats = await filterByResolution(remainingFormats)
 
-	remainingFormats = await filterByExtension(remainingFormats)
+	remainingFormats = await questions.filterByProperty(
+		'Select resolution:',
+		'resolution',
+		remainingFormats
+	)
+
+	remainingFormats = await questions.filterByProperty(
+		'Select extension:',
+		'extension',
+		remainingFormats
+	)
 
 	if (remainingFormats.length > 1) {
-		return selectOne(remainingFormats)
-	} else {
-		return remainingFormats[0]
+		return questions.selectOne(remainingFormats)
 	}
+
+	return remainingFormats[0]
 }
 
 async function fetchFormatOptions(url) {
 	const spinner = ora('Loading formats').start()
-	const run = shell.exec(`youtube-dl -F "${url}"`, { silent: true })
+	const run = shell.exec(`youtube-dl -F "${url}"`, {silent: true})
 
 	if (run.code !== 0) {
 		spinner.fail('youtube-dl stopped with error:')
 		console.log(run.stdout)
 		console.error(run.stderr)
 		return null
-	} else {
-		const qualitiesOutput = run.stdout
-		spinner.stop()
-
-		const withoutLogs = skipLogsInStdout(qualitiesOutput)
-		return parseColumns(withoutLogs, {})
 	}
+
+	const qualitiesOutput = run.stdout
+	spinner.stop()
+
+	const withoutLogs = skipLogsInStdout(qualitiesOutput)
+	return parseColumns(withoutLogs, {})
 }
+
 function skipLogsInStdout(qualitiesOutput) {
 	return qualitiesOutput
 		.split('\n')
 		.filter(line => line.indexOf('[') !== 0)
-		.join('\n');
+		.join('\n')
 }
-
--- questions.js
+++ questions.js
@@ -1,45 +1,32 @@
 const inquirer = require('inquirer')
 
-exports.filterByResolution = async function (formats) {
+exports.filterByProperty = async function(message, property, list) {
 	const answers = await inquirer.prompt([
 		{
 			type: 'list',
 			name: 'q',
-			message: 'Select resolution:',
-			choices: [...new Set(formats.map(f => f.resolution))]
+			message,
+			choices: [...new Set(list.map(f => f[property]))]
 		}
 	])
-	return formats.filter(f => f.resolution === answers.q)
+	return list.filter(f => f[property] === answers.q)
 }
 
-exports.filterByExtension = async function (formats) {
-	const answers = await inquirer.prompt([
-		{
-			type: 'list',
-			name: 'q',
-			message: 'Select extension:',
-			choices: [...new Set(formats.map(f => f.extension))]
-		}
-	])
-	return formats.filter(f => f.extension === answers.q)
-
-}
-
-exports.selectOne = async function (formats) {
+exports.selectOne = async function(formats) {
 	const answers = await inquirer.prompt([
 		{
 			type: 'list',
 			name: 'q',
 			message: 'Several matches. Select an option:',
 			choices: formats.map(f => {
-				return { name: f.extension + ' | ' + f.note, value: f }
+				return {name: f.extension + ' | ' + f.note, value: f}
 			})
 		}
 	])
 	return answers.q
 }
 
-exports.askIncludeSubs = async function () {
+exports.askIncludeSubs = async function() {
 	const answers = await inquirer.prompt([
 		{
 			type: 'confirm',
@@ -50,4 +37,3 @@ exports.askIncludeSubs = async function () {
 	])
 	return answers.q
 }
-
