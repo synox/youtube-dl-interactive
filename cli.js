56edfef4c272fd8ec43342083224c655ef5c701f
Aravindo Wingeier <synox@users.noreply.github.com>
2019-05-30 16:58:02 +0800

    Feature/mix audio video (#12)
    * #8 add presets, #7 mix audio, WIP
    * refactoring
    * refactor menu
    * implement #11
    * added some unit testing
    * improved menu output
    * make it more tolerant to missing fields
    * new demo

--- cli.js
+++ cli.js
@@ -3,25 +3,27 @@
 const meow = require('meow')
 const logSymbols = require('log-symbols')
 const shell = require('shelljs')
-const ytdlApi = require('./ytdl-api')
 const ora = require('ora')
 const updateNotifier = require('update-notifier')
+const chalk = require('chalk')
+const ytdlApi = require('./ytdl-api')
 const menu = require('./menu')
 const pkg = require('./package.json')
-const chalk = require('chalk')
 
-const cli = meow(`
+const cli = meow(
+	`
 		Usage: youtube-dl-interactive URL
 
 		Options:
-		  --help, -h  output usage information
+		  --help, -h       output usage information
 		  --version        output the version number
 		  --demo           use sample data, no remote calls
 
-		`, {
+		`,
+	{
 		flags: {
 			demo: {
-				type: 'boolean',
+				type: 'boolean'
 			}
 		}
 	}
@@ -34,17 +36,23 @@ async function init(args, flags) {
 		shell.exit(1)
 	}
 
-	updateNotifier({ pkg }).notify()
+	updateNotifier({pkg}).notify()
 
 	if (flags.demo) {
-		console.log(logSymbols.warning, chalk.bgYellowBright('Running demo with local data, not making remote calls'))
-		await run(null, true);
+		console.log(
+			logSymbols.warning,
+			chalk.bgYellowBright(
+				'Running demo with sample data, not actually calling youtube-dl.'
+			)
+		)
+		await run(null, true)
 	} else {
 		if (args.length !== 1) {
 			cli.showHelp(1)
 		}
+
 		const url = args[0]
-		await run(url, false);
+		await run(url, false)
 	}
 }
 
@@ -54,26 +62,33 @@ init(cli.input, cli.flags).catch(error => {
 })
 
 async function run(url, isDemo) {
-	let info = isDemo
+	const info = isDemo
 		? require('./test/samples/thankyousong.json')
 		: await fetchInfo(url)
 
 	if (!info) {
 		return
 	}
+
 	console.log(chalk.bold('Title:', chalk.blue(info.title)))
-	
-	const formats = info.formats
-	const { formatString, extension } = await menu.formatMenu(formats);
-	console.log(logSymbols.success, `OK, downloading format #${formatString}`);
-	let options = ` -f '${formatString}' `;
-	if (ytdlApi.supportsSubtitles(extension)) {
-		options += ' --all-subs --embed-subs ';
+
+	const {formats} = info
+	const {formatString, hasVideo, hasAudio} = await menu.formatMenu(formats)
+	let options = ` -f '${formatString}' `
+
+	if (!hasVideo && hasAudio) {
+		options += ' --extract-audio '
 	}
+
 	if (isDemo) {
-		console.log(logSymbols.warning, `End of demo. would now call: youtube-dl ${options} "${url}"`)
+		console.log(
+			logSymbols.warning,
+			`End of demo. would now call: youtube-dl ${options} <url>"`
+		)
 	} else {
-		shell.exec(`youtube-dl ${options} "${url}"`);
+		const command = `youtube-dl ${options} "${url}"`
+		console.log(logSymbols.success, `OK. Running: ${command}`)
+		shell.exec(command)
 	}
 }
 
@@ -84,12 +99,8 @@ async function fetchInfo(url) {
 		spinner.stop()
 		return info
 	} catch (error) {
-		spinner.fail('can not load formats')
+		spinner.fail('Error while loading metadata')
 		console.error(error)
 		return null
 	}
-
-
-
 }
-
--- test/ytdl-api-test.js
+++ test/ytdl-api-test.js
@@ -1,17 +1,17 @@
-import test from 'ava';
+import test from 'ava'
 import ytldlApi from '../ytdl-api'
 
-
-test.skip('parse youtube info', async t => {
-	const info = await ytldlApi.getInfo('https://www.youtube.com/watch?v=HLqIbhzrrls')
+test('parse youtube info', async t => {
+	const info = await ytldlApi.getInfo(
+		'https://www.youtube.com/watch?v=HLqIbhzrrls'
+	)
 	t.is(info.height, 1024)
-});
-
+})
 
 test('supportsSubtitles', async t => {
 	t.truthy(await ytldlApi.supportsSubtitles('mp4'))
 	t.truthy(await ytldlApi.supportsSubtitles('webm'))
 	t.truthy(await ytldlApi.supportsSubtitles('mkv'))
 	t.falsy(await ytldlApi.supportsSubtitles('mov'))
-});
-
+	t.falsy(await ytldlApi.supportsSubtitles(undefined))
+})
